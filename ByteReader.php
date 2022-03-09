82f8fea07949c28fa546c4c8aed506844e6bd1c1
clvarley <conorlukevarley@gmail.com>
2022-03-09 00:16:02 +0000

    Updated reader with new interface type

--- ByteReader.php
+++ ByteReader.php
@@ -2,7 +2,7 @@
 
 namespace Clvarley\Byte;
 
-use Clvarley\Byte\ByteString;
+use Clvarley\Byte\StreamInterface;
 
 /**
  * Utility class with methods for reading data from a byte string
@@ -11,23 +11,23 @@ Final Class ByteReader
 {
 
     /**
-     * @var ByteString $bytes
+     * @var StreamInterface $bytes Byte stream
      */
     private $bytes;
 
     /**
      * Creates a new reader around the given byte string
      *
-     * @param ByteString $bytes Byte string
+     * @param StreamInterface $bytes Byte string
      */
-    public function __construct( ByteString $bytes )
+    public function __construct( StreamInterface $bytes )
     {
         $this->bytes = $bytes;
     }
 
     public function readChar() : string
     {
-
+        return $this->bytes->read( 1 );
     }
 
     public function readInt8() : int
